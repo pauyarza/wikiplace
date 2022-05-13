33be8b83509538b77048597e2336157da2a57a3b
xpatinyo <xavierpatinyo@gmail.com>
2022-05-13 09:25:52 +0200

    ci reset

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,16 +1,17 @@
-<?php namespace Config;
+<?php
+
+namespace Config;
 
 // Create a new instance of our RouteCollection class.
 $routes = Services::routes();
 
 // Load the system's routing file first, so that the app and ENVIRONMENT
 // can override as needed.
-if (file_exists(SYSTEMPATH . 'Config/Routes.php'))
-{
-	require SYSTEMPATH . 'Config/Routes.php';
+if (file_exists(SYSTEMPATH . 'Config/Routes.php')) {
+    require SYSTEMPATH . 'Config/Routes.php';
 }
 
-/**
+/*
  * --------------------------------------------------------------------
  * Router Setup
  * --------------------------------------------------------------------
@@ -22,7 +23,7 @@ $routes->setTranslateURIDashes(false);
 $routes->set404Override();
 $routes->setAutoRoute(true);
 
-/**
+/*
  * --------------------------------------------------------------------
  * Route Definitions
  * --------------------------------------------------------------------
@@ -32,7 +33,7 @@ $routes->setAutoRoute(true);
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 
-/**
+/*
  * --------------------------------------------------------------------
  * Additional Routing
  * --------------------------------------------------------------------
@@ -45,7 +46,6 @@ $routes->get('/', 'Home::index');
  * You will have access to the $routes object within that file without
  * needing to reload it.
  */
-if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php'))
-{
-	require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
+if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
+    require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
 }
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,28 +1,11 @@
-<?php 
-
+<?php
 
 namespace App\Controllers;
 
-use App\Models\UsersModel;
-
 class Home extends BaseController
 {
-
-	//public $rutaUpload = ROOTPATH.'public/assets/client/uploads/';
-    public function __construct(){
-        helper('form');
-        helper("file");
-        $this->db = db_connect(); // Necesario para trabajar con $this->db
-        $this->db = \Config\Database::connect(); // Necesario para trabajar con el modelo
-        $this->session=\Config\Services::session();
-        $this->session->start();
+    public function index()
+    {
+        return view('welcome_message');
     }
-
-	public function index()
-	{
-		return view('homeview');
-	}
-
-	//--------------------------------------------------------------------
-
 }
