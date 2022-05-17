b33fd41cc36c1706c3a6cc6481aca12589471a6a
pauyarza <yarzapau@gmail.com>
2022-05-17 12:06:50 +0200

    modal register login 2

--- app/Views/pages/index.php
+++ app/Views/pages/index.php
@@ -10,5 +10,9 @@
 <?= $this->section('content') ?>
 
 <h1 class="indexh1">wikiplace.org</h1>
+<!--Typer-->
+<h2><span class="ityped"></span></h2>
+<!-- Necessary for typer --><script src="https://unpkg.com/ityped@0.0.10"></script>
+<!-- JS Menu --><script src="<?php echo base_url('js/menu.js'); ?>" crossorigin="anonymous"></script>
 
 <?= $this->endSection('content') ?>
--- app/Views/parts/menu.php
+++ app/Views/parts/menu.php
@@ -26,11 +26,7 @@
         ?>
     </ul>
 </nav>
-<!--Typer-->
-<h2><span class="ityped"></span></h2>
-<!-- Necessary for typer --><script src="https://unpkg.com/ityped@0.0.10"></script>
-<!-- CSS Menu --><link rel="stylesheet" src="<?php echo base_url('css/general.css'); ?>">
-<!-- JS Menu --><script src="<?php echo base_url('js/menu.js'); ?>" crossorigin="anonymous"></script>
+
 
 
 
