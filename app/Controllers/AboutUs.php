813ebea4d0f4217e656bd5bbd7f60f3a200cb1fb
xpatinyo <xavierpatinyo@gmail.com>
2022-05-18 12:18:03 +0200

    admin structure 2

--- app/Controllers/AboutUs.php
+++ app/Controllers/AboutUs.php
@@ -6,6 +6,7 @@ class AboutUs extends BaseController
 {
     public function index()
     {
+        $sessionData["is_admin"] = session()->is_admin;
         $sessionData["logged_in"] = session()->logged_in;
         $data["sessionData"] = $sessionData;
         return view("pages/about_us",$data);
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,7 @@ class Home extends BaseController
 {
     public function index()
     {
+        $sessionData["is_admin"] = session()->is_admin;
         $sessionData["logged_in"] = session()->logged_in;
         $data["sessionData"] = $sessionData;
         return view("pages/index",$data);
