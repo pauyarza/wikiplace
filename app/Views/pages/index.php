f9d0c4d6abaaf86dae8d5c810e4d3470207dc197
xpatinyo <xavierpatinyo@gmail.com>
2022-05-19 22:48:48 +0200

    index searchbar start

--- app/Views/pages/index.php
+++ app/Views/pages/index.php
@@ -10,11 +10,20 @@
 <?= $this->section('content') ?>
 
 <div class="index">
-
-    <img class="indexsvg" src="<?php echo base_url('public/img/logo.svg'); ?>" alt="wikiplace.org">
-    <!--Typer-->
-    <h2 class="typerh2"><span class="ityped"></span></h2>
-    <!-- Necessary for typer --><script src="https://unpkg.com/ityped@0.0.10"></script>
+    <div class="container index-box">
+        <img class="index-logo" src="<?php echo base_url('public/img/logo.svg'); ?>" alt="wikiplace.org">
+        <?=form_open('home/search')?>
+            <img class="search-icon" src="<?php echo base_url('public/img/search.svg'); ?>">
+            <input type="search" name="search" class="search-input">
+            <!-- <div class="search results">
+                <div>result1</div>
+                <div>result2</div>
+            </div> -->
+        </form>
+        <!--Typer-->
+        <h2 class="typer"><span class="ityped"></span></h2>
+        <!-- Necessary for typer --><script src="https://unpkg.com/ityped@0.0.10"></script>
+    </div>
 </div>
 
 <script>
